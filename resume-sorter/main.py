34fa64ccd26043b1a75a9767a1078212b4658055
Divyam Khare <66870151+dvyiam@users.noreply.github.com>
2023-07-31 21:18:05 +0530

    update main.py
    update

--- resume-sorter/main.py
+++ resume-sorter/main.py
@@ -1,5 +1,6 @@
 # import aspose.words as aw
 from pprint import pprint
+
 # doc = aw.Document("resume.pdf")
 # doc.save("u.txt")
 try:
@@ -9,17 +10,18 @@ except:
     text = []
     print("error")
 
-text_all = []
+words_all = []
 text = [elem for elem in text[2:len(text) - 1:]]
 for elem in text:
-    text_all.extend(elem.split(" "))
+    words_all.extend(elem.split(" "))
+
+words_all = [ele.split(",")[0] for ele in words_all] #this
 
 degrees = ["B.Tech", "btech", "B.TECH", "BTECH", "b.tech", "M.Tech", "mtech", "M.TECH", "MTECH", "m.tech"]
-skills = ["html","javascript", "css", "js","react.js","angular.js","express.js", "c","c++","c#","node.js","mongoDB","firebase","tailwind css","flask","django", "selenium"]
-emails = ["@gmail.com", "@yahoo.com", "@hotmail.com", "@outlook.com","@icloud.com"]
-
-
-
+skills = ["html", "javascript", "css", "js", "react.js", "angular.js", "express.js", "c", "c++", "c#", "c-sharp", "node.js",
+          "mongoDB", "firebase", "tailwind css", "flask", "django", "selenium", "java", "python"]
+skills = [ele.title() for ele in skills]
+emails = ["@gmail.com", "@yahoo.com", "@hotmail.com", "@outlook.com", "@icloud.com"]
 
 info_dict = {
     "name": text[0],
@@ -27,7 +29,8 @@ info_dict = {
     "degree_gpa": 0.0,
     "experience": [],
     "email": "",
-    "skills": 0,
+    "skills": [],
+    "skill_count": 0,
     "phone": "",
 }
 
@@ -35,8 +38,18 @@ for ele in text:
     if "CGPA" in ele:
         info_dict["degree_gpa"] = ele.split(" ")[-2]
 
-for ele in text_all:
+skill_count = 0
+
+for ele in words_all:
     if ele in degrees:
         info_dict["education_degree"].append(ele)
+    if ele.title() in skills:
+        if ele in info_dict["skills"] or ele.title() in info_dict["skills"]:
+            pass
+        else:
+            info_dict["skills"].append(ele)
+            skill_count += 1
+
+info_dict["skill_count"] = skill_count
 
 pprint(info_dict)
